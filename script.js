db9b54d60847deaf8a83eb81b734b76f08977836
Alex Burrows <burrows.alexj@gmail.com>
2024-08-22 13:43:24 -0700

    add event listeners to grid squares

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const Gameboard = (function () {
     let board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
     let gridContainer = document.querySelector(".grid-container");
 
+    // add event listeners to the grid squares
+    for(let i = 0; i < 9; i++){
+        let curChild = gridContainer.children[i];
+        curChild.addEventListener("click", function(){
+            GameController.requestMove(Math.floor(i / 3), (i % 3));
+        });
+    }
+
     const printBoard = () => {
         console.log("_" + board[0][0] + "_|_" + board[0][1] + "_|_" + board[0][2] + "_");
         console.log("_" + board[1][0] + "_|_" + board[1][1] + "_|_" + board[1][2] + "_");
@@ -11,7 +19,12 @@ const Gameboard = (function () {
         console.log("\n\n");
     }
 
-    const resetBoard = () => board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
+    const resetBoard = () => {
+        board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
+        for (let i = 0; i < 9; i++){
+            gridContainer.children[i].innerHTML = " ";
+        }
+    }
     
     const placeToken = (row, col, token) => {
         if (board[row][col] != " "){
@@ -22,7 +35,6 @@ const Gameboard = (function () {
             gridContainer.children[(3 * row) + col].innerHTML = token;
             return true;
         }
-
     }
 
     function checkWin(row, col, token) {
@@ -30,17 +42,14 @@ const Gameboard = (function () {
         if (board[row][0] == token && board[row][1] == token && board[row][2] == token){
             return true;
         }
-
         // check col
         if (board[0][col] == token && board[1][col] == token && board[2][col] == token){
             return true;
         }
-
         // check diagonal top-left to bot-right
         if ((row == col) && (board[0][0] == token && board[1][1] == token && board[2][2] == token)) {
             return true;
         }
-
         // check diagonal top-right to bot-left
         if ((row + col == 2) && (board[0][2] == token && board[1][1] == token && board[0][2] == token)){
             return true
@@ -53,7 +62,6 @@ const Gameboard = (function () {
 
 
 let GameController = (function () {
-    
 
     function createPlayer(name, value){
         const getName = () => {
@@ -74,38 +82,42 @@ let GameController = (function () {
         }
     }
 
+    function requestMove(row, col){
+        // if game is active, placeToken, check win, return true
+        if (gameOver == false){
+                return [row, col];
+        }
+        return false;
+    }
+
     const player1 = createPlayer("player1", "X");
     const player2 = createPlayer("player2", "O");
     let activePlayer = player1;
 
-    async function playGame(){
+    function playGame(){
+        Gameboard.resetBoard(); // TODO: check that this function resets the values in the html grid squares
+
         Gameboard.printBoard();
         let gameOver = false;
-        while (gameOver == false) {
-            const playerMove = prompt("" + activePlayer.getName() + " move: row, col").trim().split(",");
-            const row = parseInt(playerMove[0].trim());
-            const col = parseInt(playerMove[1].trim());
 
-            Gameboard.placeToken(row, col, activePlayer.getToken());
+        Gameboard.placeToken(row, col, activePlayer.getToken());
 
-            await sleep(100);
-            gameOver = Gameboard.checkWin(row, col, activePlayer.getToken());
-            switchPlayer();
-            Gameboard.printBoard();
-        }
+        gameOver = Gameboard.checkWin(row, col, activePlayer.getToken());
+        Gameboard.printBoard();
         switchPlayer();
         console.log("winner: " + activePlayer.getName());
     }
 
-    return {playGame};
+    return {playGame, requestMove};
 })();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 // ------------------- Game Flow --------------------------------
 
 const playGameBtn = document.getElementById("play-game-btn");
 playGameBtn.addEventListener("click", function(){
     GameController.playGame(); 
-}); //third commit test
+}); 
